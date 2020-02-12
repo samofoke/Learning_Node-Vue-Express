30537d637e9d2fe493f712ac752b0d76a759f3a2
samofoke <sabatamofokeng3@gmail.com>
2020-02-12 21:32:18 +0200

    learn3

--- javascript/bojct.js
+++ javascript/bojct.js
@@ -1,94 +1,94 @@
-//Object literals
+// //Object literals
 
-const fitData = {
-    gender: 'male',
-    age: 32,
-    totalSpeed: 262,
-    runTime: 243.5
-}
+// const fitData = {
+//     gender: 'male',
+//     age: 32,
+//     totalSpeed: 262,
+//     runTime: 243.5
+// }
 
-let data = {
-    100: 'one handred',
-    5: 'five',
-    1: 'one',
-    20: 'twenty'
-}
+// let data = {
+//     100: 'one handred',
+//     5: 'five',
+//     1: 'one',
+//     20: 'twenty'
+// }
 
-const users = {
-    username: 'sly',
-    email: 'sly@gmail.com',
-    notification: ['hi there']
-};
+// const users = {
+//     username: 'sly',
+//     email: 'sly@gmail.com',
+//     notification: ['hi there']
+// };
 
-if(users.notification.length === 0){
-    console.log('no new notifications available.');
-}
-else{
-    console.log('nottication active.');
-}
+// if(users.notification.length === 0){
+//     console.log('no new notifications available.');
+// }
+// else{
+//     console.log('nottication active.');
+// }
 
-for(let x = 0; x <= 5; x++){
-    console.log(`${x} * ${x} = ${x * x}`);
-}
+// for(let x = 0; x <= 5; x++){
+//     console.log(`${x} * ${x} = ${x * x}`);
+// }
 
-const myStudents = [
-    {
-        firstname: 'mpho',
-        marks: 64
-    },
-    {
-        firstname: 'sabata',
-        marks: 52
-    },
-    {
-        firstname: 'sipho',
-        marks: 86
-    },
-    {
-        firstname: 'sello',
-        marks: 70
-    }
-];
+// const myStudents = [
+//     {
+//         firstname: 'mpho',
+//         marks: 64
+//     },
+//     {
+//         firstname: 'sabata',
+//         marks: 52
+//     },
+//     {
+//         firstname: 'sipho',
+//         marks: 86
+//     },
+//     {
+//         firstname: 'sello',
+//         marks: 70
+//     }
+// ];
 
-let total = 0;
+// let total = 0;
 
-for(let x = 0; x < myStudents.length; x++){
-    let student = myStudents[x];
-    console.log(`${student.firstname} scored ${student.marks}`);
-    total = total + student.marks;
-}
-//building a game board
-const gameBoard = [
-    [4, 8, 12, 1],
-    [15, 3, 6, 20],
-    [7, 14, 4, 3],
-    [2, 25, 16, 0]
-];
+// for(let x = 0; x < myStudents.length; x++){
+//     let student = myStudents[x];
+//     console.log(`${student.firstname} scored ${student.marks}`);
+//     total = total + student.marks;
+// }
+// //building a game board
+// const gameBoard = [
+//     [4, 8, 12, 1],
+//     [15, 3, 6, 20],
+//     [7, 14, 4, 3],
+//     [2, 25, 16, 0]
+// ];
 
-let totalScore = 0;
+// let totalScore = 0;
 
-for(let x = 0; x < gameBoard.length; x++){
+// for(let x = 0; x < gameBoard.length; x++){
     
-    let row = gameBoard[x];
-    for(let y = 0; y < row.length; y++){
-        totalScore = totalScore + row[y];
-    }
-}
+//     let row = gameBoard[x];
+//     for(let y = 0; y < row.length; y++){
+//         totalScore = totalScore + row[y];
+//     }
+// }
 
-//buidling a math random function
-let guess;
+// //buidling a math random function
+// let guess;
 
-const target = Math.floor(Math.random() * 10);
+// const target = Math.floor(Math.random() * 10);
 
-while (guess !== target){
-    console.log(`target: ${guess} guess: ${target}`);
-    guess = Math.floor(Math.random() * 10);
-}
+// while (guess !== target){
+//     console.log(`target: ${guess} guess: ${target}`);
+//     guess = Math.floor(Math.random() * 10);
+// }
 
 
-console.log(`target: ${guess} guess: ${target}`);
-console.log('Nice job you wing...');
-console.log(totalScore);
-console.log(total);
-console.log(total/myStudents.length);
-console.log(data);
+// console.log(`target: ${guess} guess: ${target}`);
+// console.log('Nice job you wing...');
+// console.log(totalScore);
+// console.log(total);
+// console.log(total/myStudents.length);
+// console.log(data);
--- javascript/func.js
+++ javascript/func.js
@@ -1,1 +1,61 @@
 //building called function
+
+function rollDice() {
+    let roll = Math.floor(Math.random() * 6) + 1;
+    console.log(`result: ${roll}`);
+}
+
+function howMany() {
+    let x = 0;
+    while (x < 6) {
+        rollDice();
+        x++;
+    }
+    if(x % 3 == 0) {
+        console.log('SUCESS..');
+    }
+    else {
+        console,log('TRY AGAIN')
+    }
+}
+
+//howMany();
+
+//Password validator
+const details = {
+    password: 'Sly@#$1234',
+    Username: 'Slimy'
+};
+
+function isValidPassword(pass, userName) {
+    if (pass.length < 8 && pass.indexOf(' ') !== -1 && pass.indexOf(userName) !== -1) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+//Building an avarage calc
+
+function avg(arr) {
+    let total = 0;
+
+    for (let n of arr) {
+        total = total + n;
+    }
+    let res = total / arr.length;
+    return res;
+}
+
+//is pangram 
+
+function findMe(sen) {
+    let low = sen.toLowerCase();
+    for (let char of 'abcdefghijklmnopqrstuvwxyz') {
+        if (low.indexOf(char) === -1) {
+            return false;
+        }
+    }
+    return true;
+}
